c79907c76737336674d5992975189aad752e4eb8
Gil Lima <gilcpd2@gmail.com>
2017-12-08 15:09:50 +0000

    devise setup for general users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,11 @@
-
 // app/assets/javascripts/application.js
-
 //= require jquery
+// ***********************************
+// other plugins can be added here
+
+
+// but not after here - ** per default
+// ***********************************
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
